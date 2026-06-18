146de8539acfee8a7675fd8f8ae0459d16db1720
VedantSingh4108 <vedantsingh4108@gmail.com>
2026-06-18 17:39:29 +0530

    another fix to email problem

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,19 +1,17 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
-    // 1. Create the transporter (The Mail Truck)
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
         port: 465,
         secure: true,
-        family: 4, // <-- This forces IPv4 and fixes the ENETUNREACH crash!
+        family: 4,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
         },
     });
 
-    // 2. Define the email options (The Letter)
     const mailOptions = {
         from: `Helpdesk Support <${process.env.EMAIL_USER}>`,
         to: options.email,
@@ -21,7 +19,6 @@ const sendEmail = async (options) => {
         text: options.message,
     };
 
-    // 3. Send the email
     await transporter.sendMail(mailOptions);
 };
 
